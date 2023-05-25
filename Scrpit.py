b72d3cd6e9102acd67ab8b461c807967802ae645
akshay0570 <100024873+Akshay0570@users.noreply.github.com>
2023-05-25 15:01:09 +0530

    Update Scrpit.py

--- Scrpit.py
+++ Scrpit.py
@@ -7,8 +7,8 @@ url = 'http://3.135.238.207:8080/api/v2/findings/?tags=&test__tags=BUILD_ID'
 headers = {'content-type': 'application/json',
 'Authorization': 'Token 04a3f27e413800d03838d1d5ac9c5dcdb91e672b'}
 r = requests.get(url, headers=headers, verify=True) # set verify to False if ssl cert is self-signed
-#print (r.json())
-#y=json.loads(r.json())
+print (r.json())
+y=json.loads(r.json())
  
 test_txt = r.json()
 count_high = 0
@@ -28,7 +28,7 @@ for i in range(len(test_txt['results'])):
  
     else:
  
-        # print('there are no high/medium found so pipeline continue' )
+        print('there are no high/medium found so pipeline continue' )
  
         pass
  
